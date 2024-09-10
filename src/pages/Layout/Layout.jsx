e8ff86b143ea5f23bf481197048c501d83521378
sama <samaabed69@gmail.com>
2024-09-10 19:26:30 +0300

    feat: controlling content direction based on choosen language

--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -3,10 +3,19 @@ import Footer from "../../components/common/Footer";
 import { Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Loading from "../../components/common/Loading";
+import { useEffect } from "react";
+import Cookies from "js-cookie";
 
 const Layout = () => {
 
   const loading = useSelector((state)=>state.loader.loading);
+
+  useEffect(() =>{
+    const lang = Cookies.get("i18next");
+    const dir = lang == "ar" ? "rtl" : "ltr";
+    document.documentElement.setAttribute('dir', dir);
+  }, []);
+
   return (
     <>
     { loading && <Loading />}
